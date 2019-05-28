4562ecd5e7cb1a55b1c0e7d0aa96f08454e65596
DarthAV <Ansh.Verma22@outlook.com>
2019-05-28 13:40:49 -0700

    Added the coordinate class for use everywhere

---
+++ Coordinate.java
@@ -0,0 +1,15 @@
+//use this image as a reference
+//		http://bit.ly/ChessLetters
+public class Coordinate {
+	
+	private char letter;
+	private char number;
+	
+	public Coordinate(char letter, char number) {
+		this.letter = letter;
+		this.number = number;
+	}
+	public char getLetter() { return letter; }
+	public char getNumber() { return number; }
+	
+}
