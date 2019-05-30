18a3d76f7a0895f6d44d9c113c4dbdd3e0721c2c
PerpetualCreativity <47309279+PerpetualCreativity@users.noreply.github.com>
2019-05-30 13:52:09 -0700

    Update Piece.java

--- src/Piece.java
+++ src/Piece.java
@@ -1,8 +1,11 @@
-
 public abstract class Piece {
+	private char name;
+	private Coordinate spot;
 	
 	public abstract void move();
 	public abstract char getChar();
+	public abstract Coordinate[] getValidMoves();
 	
 	
-}	
+	
+}
