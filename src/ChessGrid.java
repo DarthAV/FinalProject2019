c129d32fa051f763b4241f3750c6eb6af1692762
DarthAV <Ansh.Verma22@outlook.com>
2019-06-04 12:09:42 -0700

    Colorized the grid

--- src/ChessGrid.java
+++ src/ChessGrid.java
@@ -7,26 +7,30 @@ public class ChessGrid {
 		f = new JFrame();  
 		f.setTitle("Chess Game");
 		
-		JButton[] buttons = new JButton[64];
+		JButton[][] buttons = new JButton[8][8];
 		
 		for(int i = 0; i < buttons.length; i++) {
-			buttons[i] = new JButton(/*i + 1 + ""*/);
+			for(int j = 0; j < buttons.length; j++) {
+				buttons[i][j] = new JButton(/*i + 1 + ""*/);
+			}
 		}
-		
 		
 		for(int i = 0; i < buttons.length; i++) {
-			if(i == 0) 
-				buttons[i].setBackground(Color.BLACK);
-			
-			else if(buttons[i-1].getBackground().equals(Color.BLACK))
-				buttons[i].setBackground(Color.WHITE);
-			else
-				buttons[i].setBackground(Color.BLACK);
+			for(int j = 0; j < buttons.length; j++) {
+				if ((i+j) % 2 == 0) {
+					buttons[i][j].setBackground(Color.WHITE);
+		        } else {
+		        	buttons[i][j].setBackground(Color.BLACK);
+		        }
+			}
 		}
 		
-		for(JButton i : buttons) {
-			f.add(i);
+		for(int i = 0; i < buttons.length; i++) {
+			for(int j = 0; j < buttons.length; j++) {
+				f.add(buttons[i][j]);
+			}
 		}
+		
 		f.setLayout(new GridLayout(8,8));  
 
 		f.setSize(900,900);  
