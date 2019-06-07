9c8d0e0404a15ce8982522c3b25d7ece53e2a49f
DarthAV <Ansh.Verma22@outlook.com>
2019-06-06 22:39:19 -0700

    renamed the ChessGraphics class

--- src/src/Piece.java
+++
@@ -1,9 +0,0 @@
-
-// we can always change to abstract class later
-
-public interface Piece {
-    public boolean validateMove(); // should this return a Point instead?
-    public int[][] getValidMoves(Board board); // should this return an ArrayList<Point> instead?
-    public char getChar();
-    public boolean isWhite();
-}
