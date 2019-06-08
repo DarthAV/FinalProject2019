b2e92d3d34bad9a4dece2f00366c5cdf18659633
DarthAV <Ansh.Verma22@outlook.com>
2019-06-07 23:57:42 -0700

    fixed comments

--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,19 @@ public class Main {
 		Board b = new Board();
 		b.resetBoard();
 		b.drawBoard();
-		/* Tests */
+		/* Tests:
+		 * 
+		 * put a comment that says "should be invalid" next to all
+		 * tests that should fail
+		 *  
+		 */
 		// White Pawn
 		b.move(new Point(4, 6), new Point(4, 4));
 		b.move(new Point(4, 4), new Point(4, 3));
-		b.move(new Point(4, 3), new Point(5, 2));
+		b.move(new Point(4, 3), new Point(5, 2)); //should be invalid
 		b.move(new Point(4, 3), new Point(4, 2));
 		b.move(new Point(4, 2), new Point(5, 1));
-		b.move(new Point(4, 2), new Point(5, 1));
+		b.move(new Point(4, 2), new Point(5, 1)); //should be invalid
 		b.move(new Point(5, 1), new Point(6, 0)); //king is not going into check?
 		// Black Pawn
 		b.move(new Point(4, 1), new Point(4, 3));
@@ -21,7 +26,7 @@ public class Main {
 		b.move(new Point(4, 5), new Point(4, 6));
 		b.move(new Point(4, 6), new Point(3, 7));
 
-		// Black King
-		b.move(new Point(5, 0), new Point(6, 1));
+		// White King
+		b.move(new Point(4, 7), new Point(3, 7)); 
 	}
 }
